067c8a81ca5f4ff07e9743321d9c9ff14922efbd
cisu <49948070+cisu@users.noreply.github.com>
2020-12-29 19:27:28 +0200

    Linking to the MovieForm

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {Route, Redirect, Switch} from 'react-router-dom';
 import Movies from './components/movies';
 import Customers from './components/customers';
+import MovieForm from './components/movieForm';
 import Rentals from './components/rentals';
 import NotFound from './components/notFound';
 import NavBar from './components/NavBar';
@@ -13,6 +14,7 @@ function App() {
       <NavBar />
       <main className='container'>
         <Switch>
+          <Route path='/movies/:id' component={MovieForm}></Route>
           <Route path='/movies' component={Movies}></Route>
           <Route path='/customers' component={Customers}></Route>
           <Route path='/rentals' component={Rentals}></Route>
