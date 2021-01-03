48ef0ee492264c7127724f54bd10278593d2ab23
cisu <49948070+cisu@users.noreply.github.com>
2021-01-03 23:25:22 +0200

    Refs

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -36,6 +36,11 @@ const NavBar = () => {
                 Rentals
               </NavLink>
             </li>
+            <li>
+              <NavLink className='nav-link' to='/login'>
+                Login
+              </NavLink>
+            </li>
           </ul>
         </div>
       </div>
